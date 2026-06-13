c1f5da14ac8f9bf16533ab4454d9ea2c8d253a71
RykerWilder <fabiomasciarelliwd@gmail.com>
2026-06-13 14:22:29 +0200

    fix: messages printing

--- setup.sh
+++ setup.sh
@@ -9,26 +9,26 @@ if command -v python3 >/dev/null 2>&1; then
 elif command -v python >/dev/null 2>&1; then
   PYTHON_BIN="python"
 else
-  echo "${RED}[ERROR]: Python not found${NC}"
+  echo -e "${RED}[ERROR]: Python not found${NC}"
   exit 1
 fi
 
-echo "${YELLOW}[INFO] Creating virtual environment"
+echo -e "${YELLOW}[INFO] Creating virtual environment"
 "$PYTHON_BIN" -m venv .venv
 
-echo "${YELLOW}[INFO] Activating virtual environment"
+echo -e "${YELLOW}[INFO] Activating virtual environment"
 source .venv/bin/activate
 
-echo "${YELLOW}[INFO] Upgrading pip"
+echo -e "${YELLOW}[INFO] Upgrading pip"
 python -m pip install --upgrade pip
 
-echo "${YELLOW}[INFO] Installing dependencies"
+echo -e "${YELLOW}[INFO] Installing dependencies"
 pip install -r requirements.txt
 pip install -e .
 
-echo "${GREEN}[SUCCESS] REFLEX READY"
+echo -e "${GREEN}[SUCCESS] REFLEX READY"
 
-echo "${YELLOW}[INFO] To start reflex run:
+echo -e "${YELLOW}[INFO] To start reflex run:
 - source .venv/bin/activate
 - reflex --help
 "
